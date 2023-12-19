cbdcae616e474c9c934d37f5453aa5c6c9e694d4
dmitriy-stativka <dimastativka@gmail.com>
2023-12-19 17:07:15 +0200

    added back end part for Contact, Solutions and Privacy Policy pages

--- wp-content/themes/westmount/template_parts/article_content_section.php
+++ wp-content/themes/westmount/template_parts/article_content_section.php
@@ -2,136 +2,44 @@
     <div class="container">
         <div class="article-content__inner" data-tabs-init>
             <ul class="tabs-nav">
-                <li class="tabs-nav__item">
-                    <button class="tabs-nav__btn active default-button" type="button" data-tab="1">
-                        Privacy Policy
-                    </button>
-                </li>
-                <li class="tabs-nav__item">
-                    <button class="tabs-nav__btn default-button" type="button" data-tab="2">
-                        TERMS AND CONDITIONS
-                    </button>
-                </li>
+                <?php if ( have_rows( 'tab' ) ) : ?>
+                    <?php $i = 1; while ( have_rows( 'tab' ) ) :
+                        the_row(); ?>
+                        <li class="tabs-nav__item">
+                            <button class="tabs-nav__btn default-button <?php if($i == 1){ echo 'active'; }?>" type="button" data-tab="<?php echo $i;?>">
+                                <?php if ( $tab_title = get_sub_field( 'tab_title' ) ) : ?>
+                                    <?php echo esc_html( $tab_title ); ?>
+                                <?php endif; ?>
+                            </button>
+                        </li>
+                    <?php $i++; endwhile; ?>
+                <?php endif; ?>
             </ul>
             <ul class="tabs-content">
-                <li class="tabs-content__item active" data-tab-content="1">
-                    <ul class="accordion" data-default="1" data-single="true" data-breakpoint="9999" data-accordion>
-                        <li class="accordion__item">
-                            <button class="accordion__btn" data-id="1">
-                                Introduction
-                            </button>
-                            <div class="accordion__content" data-content="1">
-                                <div class="article-content__text">
-                                    <p>
-                                        We at Westmount Guarantee Services Inc. (“Westmount”, “we”, “us”, “our”)
-                                        recognize the importance of privacy and the sensitivity of personal information.
-                                        We are committed to protecting the confidentiality of personal information
-                                        provided to us and ensuring that all personal information we hold is collected,
-                                        used and disclosed in compliance with all applicable federal and provincial
-                                        privacy requirements. This Privacy Policy outlines how we manage your personal
-                                        information and safeguard your privacy.
-                                    </p>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="accordion__item">
-                            <button class="accordion__btn" data-id="2">
-                                Your Privacy Rights
-                            </button>
-                            <div class="accordion__content" data-content="2">
-                                <div class="article-content__text">
-                                    <p>
-                                        We at Westmount Guarantee Services Inc. (“Westmount”, “we”, “us”, “our”)
-                                        recognize the importance of privacy and the sensitivity of personal information.
-                                        We are committed to protecting the confidentiality of personal information
-                                        provided to us and ensuring that all personal information we hold is collected,
-                                        used and disclosed in compliance with all applicable federal and provincial
-                                        privacy requirements. This Privacy Policy outlines how we manage your personal
-                                        information and safeguard your privacy.
-                                    </p>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="accordion__item">
-                            <button class="accordion__btn" data-id="3">
-                                Scope Of This Privacy Policy
-                            </button>
-                            <div class="accordion__content" data-content="3">
-                                <div class="article-content__text">
-                                    <p>
-                                        We at Westmount Guarantee Services Inc. (“Westmount”, “we”, “us”, “our”)
-                                        recognize the importance of privacy and the sensitivity of personal information.
-                                        We are committed to protecting the confidentiality of personal information
-                                        provided to us and ensuring that all personal information we hold is collected,
-                                        used and disclosed in compliance with all applicable federal and provincial
-                                        privacy requirements. This Privacy Policy outlines how we manage your personal
-                                        information and safeguard your privacy.
-                                    </p>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </li>
-
-
-                <li class="tabs-content__item" data-tab-content="2">
-                    <ul class="accordion" data-default="1" data-single="true" data-breakpoint="9999" data-accordion>
-                        <li class="accordion__item">
-                            <button class="accordion__btn" data-id="1">
-                                Introduction
-                            </button>
-                            <div class="accordion__content" data-content="1">
-                                <div class="article-content__text">
-                                    <p>
-                                        We at Westmount Guarantee Services Inc. (“Westmount”, “we”, “us”, “our”)
-                                        recognize the importance of privacy and the sensitivity of personal information.
-                                        We are committed to protecting the confidentiality of personal information
-                                        provided to us and ensuring that all personal information we hold is collected,
-                                        used and disclosed in compliance with all applicable federal and provincial
-                                        privacy requirements. This Privacy Policy outlines how we manage your personal
-                                        information and safeguard your privacy.
-                                    </p>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="accordion__item">
-                            <button class="accordion__btn" data-id="2">
-                                Your Privacy Rights
-                            </button>
-                            <div class="accordion__content" data-content="2">
-                                <div class="article-content__text">
-                                    <p>
-                                        We at Westmount Guarantee Services Inc. (“Westmount”, “we”, “us”, “our”)
-                                        recognize the importance of privacy and the sensitivity of personal information.
-                                        We are committed to protecting the confidentiality of personal information
-                                        provided to us and ensuring that all personal information we hold is collected,
-                                        used and disclosed in compliance with all applicable federal and provincial
-                                        privacy requirements. This Privacy Policy outlines how we manage your personal
-                                        information and safeguard your privacy.
-                                    </p>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="accordion__item">
-                            <button class="accordion__btn" data-id="3">
-                                Scope Of This Privacy Policy
-                            </button>
-                            <div class="accordion__content" data-content="3">
-                                <div class="article-content__text">
-                                    <p>
-                                        We at Westmount Guarantee Services Inc. (“Westmount”, “we”, “us”, “our”)
-                                        recognize the importance of privacy and the sensitivity of personal information.
-                                        We are committed to protecting the confidentiality of personal information
-                                        provided to us and ensuring that all personal information we hold is collected,
-                                        used and disclosed in compliance with all applicable federal and provincial
-                                        privacy requirements. This Privacy Policy outlines how we manage your personal
-                                        information and safeguard your privacy.
-                                    </p>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </li>
+                <?php if ( have_rows( 'tab' ) ) : ?>
+                    <?php $i = 1; while ( have_rows( 'tab' ) ) :
+                        the_row(); ?>
+                            <li class="tabs-content__item <?php if($i == 1){ echo 'active'; }?>" data-tab-content="<?php echo $i;?>">
+                                <ul class="accordion" data-default="1" data-single="true" data-breakpoint="9999" data-accordion>
+                                    <?php if ( have_rows( 'tab_content' ) ) : ?>
+                                        <?php $index = 1; while ( have_rows( 'tab_content' ) ) :
+                                            the_row(); ?>
+                                            <li class="accordion__item">
+                                                <button class="accordion__btn" data-id="<?php echo $index;?>"> <?php if ( $title = get_sub_field( 'title' ) ) : echo esc_html( $title ); endif; ?></button>
+                                                <div class="accordion__content" data-content="<?php echo $index;?>">
+                                                    <div class="article-content__text">
+                                                        <p>
+                                                            <?php if ( $description = get_sub_field( 'description' ) ) : echo $description; endif; ?>
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        <?php $index++; endwhile; ?>
+                                    <?php endif; ?>
+                                </ul>
+                            </li>
+                    <?php $i++; endwhile; ?>
+                <?php endif; ?>
             </ul>
         </div>
     </div>
