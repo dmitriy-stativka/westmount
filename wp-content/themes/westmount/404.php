b9656858671936937b7fa2f53cabb92f698b96a2
Soin Andrey <soinproduction@gmail.com>
2023-12-25 13:12:44 +0200

    add 404

--- wp-content/themes/westmount/404.php
+++ wp-content/themes/westmount/404.php
@@ -1,12 +1,29 @@
+
+<?php $build_folder = get_template_directory_uri() . '/assets/'?>
 <?php get_header() ?>
 
-    <div class="grid place-content-center h-full text-center">
+    <section class="contact-section error">
+        <div class="container">
+            <div class="contact-section__error">
 
-        <h1 class="text-5xl font-semibold">404</h1>
-        <div class="mt-4">
-            <h2>Not found</h2>
+                <div class="main-top">
+                    <h1 class="main-top__title">404</h1>
+                    <h2 class="main-top__subtitle">Page not found</h2>
+
+                    <a href="/" class="action_btn">
+                        <span class="action_btn_text">Home</span>
+                        <div class="square">
+                            <svg width="11" height="14">
+                                <use href="<?php echo $build_folder ?>img/sprite/sprite.svg#arrow-r"></use>
+                            </svg>
+                        </div>
+                    </a>
+                </div>
+            </div>
         </div>
 
-    </div>
+
+
+    </section>
 
 <?php get_footer() ?>
