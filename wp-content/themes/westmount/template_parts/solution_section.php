8468351230c1b0d9f67a47df056337520b31f1b9
dmitriy-stativka <dimastativka@gmail.com>
2023-12-26 23:07:44 +0200

    added hide block

--- wp-content/themes/westmount/template_parts/solution_section.php
+++ wp-content/themes/westmount/template_parts/solution_section.php
@@ -29,49 +29,52 @@
                 <?php if ( have_rows( 'solutions_list' ) ) : ?>
                     <?php while ( have_rows( 'solutions_list' ) ) :
                         the_row(); ?>
-                        <li class="card-list__item 
-                            <?php if ( get_sub_field( 'width_item' ) == '33%'){
-                                    echo "-small";
-                                }elseif(get_sub_field( 'width_item' ) == '50%'){
-                                    echo "-medium";
-                                }
-                                elseif(get_sub_field( 'width_item' ) == '100%'){
-                                    echo "-large";
-                                }
-                            ?>"
-                        >
-                            <article class="card-list__article">
-                                <div class="card-list__article-img">
-                                    <img src="<?php echo esc_url( get_sub_field( 'image' )['sizes']['1440'] ); ?>" alt="">
-                                </div>
 
-                                <div class="card-list__article-inner">
-                                    <?php if ( $title = get_sub_field( 'title' ) ) : ?>
-                                        <h3 class="card-list__article-title" <?php if ( $sub_title = get_sub_field( 'sub_title' ) ) : ?> data-descr="<?php echo esc_html( $sub_title );?>" <?php endif; ?>><?php echo esc_html( $title ); ?></h3>
-                                    <?php endif; ?>
+                        <?php if(!get_sub_field('hide_block')): ?>
+                            <li class="card-list__item 
+                                <?php if ( get_sub_field( 'width_item' ) == '33%'){
+                                        echo "-small";
+                                    }elseif(get_sub_field( 'width_item' ) == '50%'){
+                                        echo "-medium";
+                                    }
+                                    elseif(get_sub_field( 'width_item' ) == '100%'){
+                                        echo "-large";
+                                    }
+                                ?>">
+                                <article class="card-list__article">
+                                    <div class="card-list__article-img">
+                                        <img src="<?php echo esc_url( get_sub_field( 'image' )['sizes']['1440'] ); ?>" alt="">
+                                    </div>
 
-                                    <?php if ( $description = get_sub_field( 'description' ) ) : ?>
-                                        <p><?php echo $description; ?> </p>
-                                    <?php endif; ?>
+                                    <div class="card-list__article-inner">
+                                        <?php if ( $title = get_sub_field( 'title' ) ) : ?>
+                                            <h3 class="card-list__article-title" <?php if ( $sub_title = get_sub_field( 'sub_title' ) ) : ?> data-descr="<?php echo esc_html( $sub_title );?>" <?php endif; ?>><?php echo esc_html( $title ); ?></h3>
+                                        <?php endif; ?>
 
-                                    <?php
-                                        $link = get_sub_field( 'link' );
-                                        if ( $link ) :
-                                            $link_url = $link['url'];
-                                            $link_title = $link['title'];
-                                            $link_target = $link['target'] ? $link['target'] : '_self';
-                                            ?>
-                                            <a class="card-list__article-link arrow-link" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>">
-                                                <?php echo esc_html( $link_title ); ?> 
-                                                <svg width="14" height="11">
-                                                    <use href="<?php echo $build_folder ?>img/sprite/sprite.svg#arrow-r"></use>
-                                                    </svg>
-                                            </a>
-                                        <?php endif; 
-                                    ?>
-                                </div>
-                            </article>
-                        </li>
+                                        <?php if ( $description = get_sub_field( 'description' ) ) : ?>
+                                            <p><?php echo $description; ?> </p>
+                                        <?php endif; ?>
+
+                                        <?php
+                                            $link = get_sub_field( 'link' );
+                                            if ( $link ) :
+                                                $link_url = $link['url'];
+                                                $link_title = $link['title'];
+                                                $link_target = $link['target'] ? $link['target'] : '_self';
+                                                ?>
+                                                <a class="card-list__article-link arrow-link" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>">
+                                                    <?php echo esc_html( $link_title ); ?> 
+                                                    <svg width="14" height="11">
+                                                        <use href="<?php echo $build_folder ?>img/sprite/sprite.svg#arrow-r"></use>
+                                                        </svg>
+                                                </a>
+                                            <?php endif; 
+                                        ?>
+                                    </div>
+                                </article>
+                            </li>
+                        <?php endif; ?>   
+
                     <?php endwhile; ?>
                 <?php endif; ?>   
             </ul>
